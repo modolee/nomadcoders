0459cf4e662a1a4eee51ed7fd43efae4789a038f
modolee <modolee.si@gmail.com>
2018-11-18 18:15:57 +0900

    keyword 'then's were replaced with async and await for readability

--- js-clock/weather.js
+++ js-clock/weather.js
@@ -3,17 +3,17 @@ const weather = document.querySelector(".js-weather");
 const API_KEY = "";
 const COORDS = "coords";
 
-function getWeather(lat, lon) {
-  fetch(
+async function getWeather(lat, lon) {
+  response = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
-  ).then(function(response) {
-    return response.json();
-  }).then(function(json){
-    const temperature = json.main.temp;
-    const place = json.name;
-    weather.innerHTML = `${temperature} @ ${place}`
-    console.log(json);
-  });
+  );
+  console.log(response);
+  json = await response.json();
+  
+  const temperature = json.main.temp;
+  const place = json.name;
+  weather.innerHTML = `${temperature} @ ${place}`
+  
 }
 
 function saveCoords(coordsObj) {
