8a1cf2242824dafa7301fcfcc77447dbe40c9009
modolee <modolee.si@gmail.com>
2018-11-18 17:15:06 +0900

    added geolocation functions for weather

---
+++ js-clock/weather.js
@@ -0,0 +1,39 @@
+const API_KEY = "898cc0e90110fd9b57f8b613330cac25";
+const COORDS = "coords";
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude
+  }
+  saveCoords(coordsObj);
+}
+
+function handleGeoError() {
+  console.log("Can't access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
