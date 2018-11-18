984666201fdb8a244550d3025ed9843d2e4dd912
modolee <modolee.si@gmail.com>
2018-11-18 17:45:08 +0900

    added weather api

--- js-clock/weather.js
+++ js-clock/weather.js
@@ -1,5 +1,20 @@
-const API_KEY = "898cc0e90110fd9b57f8b613330cac25";
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "";
 const COORDS = "coords";
+
+function getWeather(lat, lon) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+  ).then(function(response) {
+    return response.json();
+  }).then(function(json){
+    const temperature = json.main.temp;
+    const place = json.name;
+    weather.innerHTML = `${temperature} @ ${place}`
+    console.log(json);
+  });
+}
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -13,6 +28,7 @@ function handleGeoSuccess(position) {
     longitude
   }
   saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -28,7 +44,8 @@ function loadCoords() {
   if (loadedCoords === null) {
     askForCoords();
   } else {
-
+    const parseCoords = JSON.parse(loadedCoords);
+    getWeather(parseCoords.latitude, parseCoords.longitude);
   }
 }
 
